674a851327a821df33f4800024e7cb9a2ea35007
InterStellar12345 <42122677+InterStellar12345@users.noreply.github.com>
2022-09-24 12:35:27 -0500

    Create TankPlease.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankPlease.java
@@ -0,0 +1,81 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="TANK", group="Iterative Opmode")
+public class TankPlease extends OpMode
+{
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    public M1_Robot_Base rb;
+    double leftPower;
+    double rightPower;
+    double trigger;
+    double K = 1.5;
+
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+        rb = new M1_Robot_Base(hardwareMap, telemetry);
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+
+
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+
+
+        // if button toggle, switch speed
+        if (gamepad1.left_bumper){
+            K = 3;
+        }
+        else if (gamepad1.right_bumper) {
+            K = 1;
+        }
+
+        leftPower = -gamepad1.left_stick_y/K;
+        rightPower = -gamepad1.right_stick_y/K;
+
+        // logical tank, mechanical strafe
+        rb.setSidePowers(leftPower, rightPower);
+
+        // logical strafe, mechanical tank
+        trigger=(gamepad1.right_trigger-gamepad1.left_trigger);
+        rb.strafe(trigger/K);
+
+
+
+        rb.performUpdates();
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
+}
