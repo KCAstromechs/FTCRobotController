995b7d96b38851cc4a7944f8718e093932431bae
Reese <audreyiswow@gmail.com>
2023-03-16 21:06:46 -0500

    Progress on QR Reader
    set-up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/QRVisionBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/QRVisionBase.java
@@ -2,15 +2,41 @@
 package org.firstinspires.ftc.teamcode;
 
 import android.graphics.Bitmap;
-import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.barcode.common.Barcode;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.common.InputImage;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+import java.util.List;
+
 public class QRVisionBase {
 
     private CameraBase cb;
+
+    //----------------------------------------------------------------------------------------------
+    // Variables
+    //----------------------------------------------------------------------------------------------
+
+    // color variables
+    enum ZONE {
+        ONE,
+        TWO,
+        THREE,
+        NOT_DETECTED
+    }
 
     // robot variables
     HardwareMap hardwareMap;
@@ -23,6 +49,7 @@ public class QRVisionBase {
     public QRVisionBase(HardwareMap _hardwareMap, Telemetry _telemetry) {
         hardwareMap = _hardwareMap;
         telemetry = _telemetry;
+        cb = new CameraBase(_hardwareMap, _telemetry);
     }
 
     // do this at the beginning
@@ -30,22 +57,45 @@ public class QRVisionBase {
         cb.initCamera();
     }
 
-    public int findZone(int minX, int maxX, int minY, int maxY, boolean save) {
-        int color = 0;
-        int redValue = 0;
-        int greenValue = 0;
-        int blueValue = 0;
-        int pixelCountR = 0;
-        int pixelCountG = 0;
-        int pixelCountB = 0;
-        int detectionThreshold = 50;
-        int minColorDifference = 20;
-
+    public ZONE findZone(int minX, int maxX, int minY, int maxY, boolean save) {
+        // set-up barcode scanning
+        BarcodeScannerOptions options =
+                new BarcodeScannerOptions.Builder()
+                        .setBarcodeFormats(
+                                Barcode.FORMAT_QR_CODE)
+                        .build();
+        BarcodeScanner scanner = BarcodeScanning.getClient();
         // retrieve bitmap
         Bitmap bitmap = cb.returnBitmap(minX, maxX, minY, maxY, save);
         if (bitmap == null) {
-            return 0;
+            return ZONE.NOT_DETECTED;
         }
-        return 1;
+        // scan barcode
+        InputImage image = InputImage.fromBitmap(bitmap, 0);
+        Task<List<Barcode>> result = scanner.process(image)
+                .addOnSuccessListener(new OnSuccessListener<List<Barcode>>() {
+                    @Override
+                    public void onSuccess(List<Barcode> barcodes) {
+                        // Task completed successfully
+                        for (Barcode barcode: barcodes) {
+                            Rect bounds = barcode.getBoundingBox();
+                            Point[] corners = barcode.getCornerPoints();
+
+                            String rawValue = barcode.getRawValue();
+
+                            int valueType = barcode.getValueType();
+                            }
+                        }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Task failed with an exception
+                        // ...
+                    }
+                });
+
+        return ZONE.ONE;
     }
+
 }
